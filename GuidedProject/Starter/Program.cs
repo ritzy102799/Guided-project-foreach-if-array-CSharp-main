4dc4ce20fda850eb3b7dc071bd870cb217de5c1f
ROVIC ROSALES <ritzy102799@gmail.com>
2024-02-22 17:37:02 +0100

    updated student  grades letter

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -9,7 +9,10 @@ int[] andrewScores  = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores    = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores   = new int[] { 90, 95, 87, 88, 96 };
 string[] studentNames = new string[] { "sophia", "andrew", "emma", "logan" };
+
 int[] studentScores = new int[10];
+string currentStudentLetterGrade = "";
+
 
 Console.WriteLine("Student\t\tGrade\n");
 
@@ -37,8 +40,36 @@ foreach (string name in studentNames)
         sumAssignmentScores += score;
     }
     currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
-    
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+   else 
+        currentStudentLetterGrade = "F";
+
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+
 }
 
 
