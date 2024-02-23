6f2511b8039e1eddb6f00d320016648b83baf49d
ROVIC ROSALES <ritzy102799@gmail.com>
2024-02-23 15:34:42 +0100

    added new ' students

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -8,9 +8,13 @@ int[] sophiaScores  = new int[] { 90, 86, 87, 98, 100, 94, 90 };
 int[] andrewScores  = new int[] { 92, 89, 81, 96, 90, 89 };
 int[] emmaScores    = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
 int[] loganScores   = new int[] { 90, 95, 87, 88, 96,96 };
+int[] beckyScores   = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores   = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores    = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores  = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 
 // Student names
-string[] studentNames = new string[] { "sophia", "andrew", "emma", "logan" };
+string[] studentNames = new string[] { "sophia", "andrew", "emma", "logan", "becky", "chris", "eric", "gregor" };
 
 int[] studentScores = new int[10];
 
@@ -34,7 +38,23 @@ foreach (string name in studentNames)
         studentScores = emmaScores;
 
     else if (currentStudent == "logan")
+
         studentScores = loganScores;
+    else if (currentStudent == "becky")
+
+        studentScores = beckyScores;
+
+    else if (currentStudent == "chris")
+
+        studentScores = chrisScores;
+
+    else if (currentStudent == "eric")
+
+        studentScores = ericScores;
+
+    else if (currentStudent == "gregor")
+
+        studentScores = gregorScores;
 
     // initialize/reset the sum of scored assignments
     int sumAssignmentScores = 0;
