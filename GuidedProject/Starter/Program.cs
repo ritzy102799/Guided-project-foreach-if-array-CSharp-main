118a1d40c052801668fa3efcf16c098f054b2cf5
ROVIC ROSALES <ritzy102799@gmail.com>
2024-02-23 15:26:54 +0100

    added 10% extra credit

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -2,23 +2,27 @@
 
 // initialize variables - graded assignments 
 
-int currentAssignments = 5;
+int examAssignments = 5;
 
-int[] sophiaScores  = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores  = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores    = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores   = new int[] { 90, 95, 87, 88, 96 };
+int[] sophiaScores  = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores  = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores    = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores   = new int[] { 90, 95, 87, 88, 96,96 };
+
+// Student names
 string[] studentNames = new string[] { "sophia", "andrew", "emma", "logan" };
 
 int[] studentScores = new int[10];
+
 string currentStudentLetterGrade = "";
 
-
+// Write the Report Header to the console
 Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
 {
     string currentStudent = name;
+
 
     if (currentStudent == "sophia")
         studentScores = sophiaScores;
@@ -32,14 +36,29 @@ foreach (string name in studentNames)
     else if (currentStudent == "logan")
         studentScores = loganScores;
 
+    // initialize/reset the sum of scored assignments
     int sumAssignmentScores = 0;
+
+    // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
 
+    // initialize/reset a counter for the number of assignment 
+    int gradedAssignments = 0;
+
+    // loop through the scores array and complete calculations for currentStudent
     foreach (int score in studentScores)
     {
-        sumAssignmentScores += score;
+        // increment the assignment counter
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+            // add the exam score to the sum
+            sumAssignmentScores += score;
+        else
+            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+            sumAssignmentScores += score / 10;
     }
-    currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+    currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
     else if (currentStudentGrade >= 93)
@@ -67,7 +86,7 @@ foreach (string name in studentNames)
    else 
         currentStudentLetterGrade = "F";
 
-
+    // required for running in VS Code (keeps the Output windows open to view results)
     Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 
 }
